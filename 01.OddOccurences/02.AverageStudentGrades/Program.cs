5e771a25964859d12da0e413549695772877de63
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2017-12-06 17:45:52 +0100

    Second Problem

---
+++ 01.OddOccurences/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.AverageStudentGrades
+{
+    class Program
+    {
+        static void Main()
+        {
+            var grades = new Dictionary<string, List<double>>();
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var name = tokens[0];
+                var grade = double.Parse(tokens[1]);
+                if (!grades.ContainsKey(name))
+                {
+                    grades[name] = new List<double>();
+                    
+                }
+                grades[name].Add(grade);
+            }
+            foreach (var pair in grades)
+            {
+                var name = pair.Key;
+                var studentsGrades = pair.Value;
+                var average = studentsGrades.Average(); //average value of the list
+
+                Console.Write($"{name} -> ");
+
+                foreach (var grade in studentsGrades)
+                {
+                    Console.Write($"{grade:f2} ");
+                    
+                }
+                Console.WriteLine($"(avg: {average:f2})");
+            }
+        }
+    }
+}
