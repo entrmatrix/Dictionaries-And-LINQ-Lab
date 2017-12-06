eec097dd9b04f525a3ab3c07cb78d0da3c7b7fd3
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2017-12-06 14:07:12 +0100

    Edit

---
+++ 01.OddOccurences/01.OddOccurences/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Odd_Occurrences
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().ToLower().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var sequenceOfWords = new Dictionary<string, int>();
+
+            foreach (var word in input)
+            {
+                if (sequenceOfWords.ContainsKey(word))
+                {
+                    sequenceOfWords[word]++;
+                }
+                else
+                {
+                    sequenceOfWords[word] = 1;
+                }
+            }
+
+            var oddCount = new List<string>(); // creating a new list which will store the odd count
+
+            foreach (var pair in sequenceOfWords)
+            {
+                if (pair.Value % 2 == 1)
+                {
+                    oddCount.Add(pair.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", oddCount)); //print the list
+
+        }
+    }
+}
